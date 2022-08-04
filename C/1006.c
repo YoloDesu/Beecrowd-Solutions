2e03da97394f3a417ce4153ec1b04bfa90cc5b82
YoloDesu <110636466+YoloDesu@users.noreply.github.com>
2022-08-04 18:51:16 -0300

    1006

---
+++ C/1006.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	double a, b, c, media;
+	scanf("%lf%lf%lf", &a, &b, &c);
+	media = (a*2 + b*3 + c*5)/10;
+	printf("MEDIA = %.1lf\n", media);
+	return 0;
+}
